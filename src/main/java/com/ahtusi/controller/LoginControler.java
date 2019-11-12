0b9a25c014783784fe0994ca9b8b0c5366349710
ahShanPeng <shanpeng@ahtusi.com>
2019-11-12 16:58:33 +0800

    项目文档完善

--- src/main/java/com/ahtusi/controller/LoginControler.java
+++ src/main/java/com/ahtusi/controller/LoginControler.java
@@ -18,9 +18,9 @@ public class LoginControler {
                           @RequestParam(value = "password", defaultValue = "")String password){
 
         if (username.equals("admin") && password.equals("admin") ){
-            final String secretId = "8778210956007260556";
-            final String secretKey = "9104D293BC764DBA8DFBC0E169753244BZY8YW5A";
-            final String userId = "tre"; //当前登录账户的userId
+            final String secretId = "5396060554446458155";
+            final String secretKey = "A0A7B879C9B244FBA1AD0822EA28C6FBBIDOY3WI";
+            final String userId = "MFATest009"; //当前登录账户的userId
             final String callbackUrl = "http://localhost:8082/mfademo_war_exploded/mfademo/result"; //用于接收MFA二次认证后的认证结果
             TUSIMfaWebClient.RequestCreator requestCreator = new TUSIMfaWebClient.RequestCreator()
                     .secretId(secretId)
@@ -29,7 +29,7 @@ public class LoginControler {
                     .callbackUrl(callbackUrl);
             String requestCode =  requestCreator.create(); //生成requestCode
             // 2.重定向到MFA:
-            return "redirect:http://localhost:8080/MFA_war_exploded/open/mfa/start?code=" + requestCode;
+            return "redirect:https://www.bebeman.com/MFA/open/mfa/binding/start?code=" + requestCode;
         }
         return "error";
     }
@@ -37,13 +37,13 @@ public class LoginControler {
     @RequestMapping("result")
     public String getResult(@RequestParam(value = "code",defaultValue = "")String code){
 //        1.解析responseCode:
-//        final String responseCode = "xxxx"; //callbackUrl中拿到的responseCode
-        final String secretKey = "9104D293BC764DBA8DFBC0E169753244BZY8YW5A";
+        final String responseCode = code; //callbackUrl中拿到的responseCode
+        final String secretKey = "A0A7B879C9B244FBA1AD0822EA28C6FBBIDOY3WI";
         TUSIMfaWebClient.ResponseVerifier verifier = new TUSIMfaWebClient.ResponseVerifier()
-                .code(code)
+                .code(responseCode)
                 .secretKey(secretKey);
         String userId = verifier.verify(); //认证成功，则返回userId
-        if (userId.equals("tre")){
+        if (userId.equals("MFATest009")){
         return "success";
         }
         return "error";
